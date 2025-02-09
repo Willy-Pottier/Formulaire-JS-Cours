91705052a939ca5125f09ed9a72850a68a20755a
Willy Pottier <wigitalagency@gmail.com>
2025-02-09 11:14:14 +0100

    Finish Formulaire

--- SCRIPT/evenements.js
+++ SCRIPT/evenements.js
@@ -1,3 +1,26 @@
 import Formulaire from "./formulaire.js"
 
 const formulaire = new Formulaire('formulaire')
+
+formulaire.maskChamp('societe')
+
+formulaire.maskChamp('email')
+
+//addEventListener pour changer le comportement du radio coché
+
+formulaire.getElement('particulier').addEventListener('change', () => {formulaire.hideChamp('societe')})
+
+formulaire.getElement('professionnel').addEventListener('change', () => {formulaire.showChamp('societe')})
+
+//addEventListener pour changer le comportement en fonction de objet
+
+formulaire.getElement('objet').addEventListener('change', () => {formulaire.isSelected('objet', "demande_de_contact", () => {formulaire.showChamp('email')}, () =>{formulaire.hideChamp('email')})})
+
+//addEventListener pour récupérer les données du formulaire
+
+formulaire.formulaireHTML.addEventListener('submit',
+    (event) => {
+        event.preventDefault()
+        formulaire.affAnswers()
+    }
+)
